1d2b39c92507b1d54ea4161bd444a2430cb8a14c
SOYHOK <100107761+SOYHOK@users.noreply.github.com>
2022-08-29 16:40:27 +0700

    committed

--- lower_and_upper.py
+++ lower_and_upper.py
@@ -1,3 +1,9 @@
+#lower to upper
 first_name=str(input("Enter your first name with smaller letter:"))
 last_name=str(input("Enter the last name with smaller letter:"))
 print(first_name.upper(),last_name.upper())
+
+#upper to lower
+first_name=str(input("Enter your first name: "))
+last_name=str(input("Enter the last name: "))
+print(first_name.lower(),last_name.lower())
